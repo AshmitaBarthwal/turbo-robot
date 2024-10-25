dcb4a8fb4afba864d3bb82b80b9659a4cd57834c
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-10-26 00:38:55 +0530

    Create Cycle Detection In LinkedList.java

---
+++ Cycle Detection In LinkedList.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class ListNode {
+    int data;
+    ListNode next;
+
+    ListNode(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LinkedListCycleDetection {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ListNode createLinkedList(int[] values) {
+        if (values.length == 0) return null;
+
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+
+        return head;
+    }
+  //main function can be written according to user needs
